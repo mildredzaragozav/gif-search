9878c106d200ddc4b7bfc6ede8abe7de3065c97d
Mildred Zaragoza <mildredzaragozav@gmail.com>
2026-04-28 09:41:30 -0600

    Adding test for CustomHeader component.

--- src/GifsApp.test.tsx
+++ src/GifsApp.test.tsx
@@ -1,8 +1,11 @@
-import { describe, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
+import { render } from '@testing-library/react'
+import { GifsApp } from './GifsApp';
 
 
 describe('GifsApp', () => {
     test('Should render component properly', () => {
-
+        const { container } = render(<GifsApp />)
+        expect(container).toMatchSnapshot();
     });
 });
